fa989cf3ad8a65cb8743af9acbedc5c306cab496
Urszula Suplicka <usuplicka@gmail.com>
2020-12-12 21:41:46 +0100

    Add kodilla consol log

---
+++ .history/js/script_20201212213356.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function titleClickHandler(){
+    console.log('Link was clicked!');
+  }
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
